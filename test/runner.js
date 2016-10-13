783d5ddf7140743f095ba5c916dd836b09ed2ec4
Sam Houston <shouston3@me.com>
2016-10-13 21:45:46 +0100

    Now testing all my helpers and using more helpers when testing the server

--- test/runner.js
+++ test/runner.js
@@ -1,1 +1,7 @@
+require('./helpers/flushDb.test.js');
+require('./helpers/checkUserRegistered.test.js');
+require('./helpers/registerUser.test.js');
+require('./helpers/checkUserLoggedIn.test.js');
+require('./helpers/loginUser.test.js');
+
 require('./server.test.js');
