f978408415216ff197dc0d9d3d7f766d3f215c74
Sam Houston <shouston3@me.com>
2016-10-13 21:46:07 +0100

    Rewriting the server.test.js file from scratch

--- test/server.test.js
+++ test/server.test.js
@@ -1,183 +1,384 @@
 const tape = require('tape');
+const assert = require('assert');
 
 const server = require('../server/server.js');
-const flushDb = require('./helpers/flushDb.js')(server.app.pool);
+const pool = server.app.pool;
+const redisCli = server.app.redisCli;
+const checkUserRegistered = require('./helpers/checkUserRegistered.js')(pool);
+const registerUser = require('./helpers/registerUser.js')(pool);
+const checkUserLoggedIn = require('./helpers/checkUserLoggedIn.js')(redisCli);
+const loginUser = require('./helpers/loginUser.js')(redisCli);
+const flushDb = require('./helpers/flushDb.js')(pool, redisCli);
 
-['/login', '/register'].forEach((route) => {
-  tape('GET :: ' + route, (t) => {
-    const options = {
-      method: 'get',
-      url: route
-    };
-    server.inject(options, (res) => {
+tape('/login :: GET', (t) => {
+  server.inject({ method: 'get', url: '/login' })
+    .then((res) => {
       t.equal(res.statusCode, 200);
-      t.ok(res.payload.includes('<!DOCTYPE'));
+      t.ok(res.payload.includes('Hello Login'));
       t.end();
-    });
-  });
+    })
+    .catch((err) => assert(!err, err));
 });
 
-tape('POST :: /register', (t) => {
-  flushDb(() => {
-    const options = {
-      method: 'post',
-      url: '/register',
-      payload: {
-        username: 'sam',
-        password: 'pass'
-      }
-    };
 
-    server.inject(options, (registerRes) => {
-      t.equal(registerRes.statusCode, 200);
-      t.ok(registerRes.payload.includes('Hello Register'));
+tape('/register :: GET', (t) => {
+  server.inject({ method: 'get', url: '/register' })
+    .then((res) => {
+      t.equal(res.statusCode, 200);
+      t.ok(res.payload.includes('Hello Register'));
       t.end();
-    });
-  });
+    })
+    .catch((err) => assert(!err, err));
 });
 
-tape('POST :: /login', (t) => {
-  flushDb(() => {
-    const registerOptions = {
-      method: 'post',
-      url: '/register',
-      payload: {
-        username: 'sam',
-        password: 'pass'
-      }
-    };
+tape('/register :: POST with no payload or payload without user or pass', (t) => {
+  const opts = { method: 'post', url: '/register' };
 
-    const loginOptions = {
-      method: 'post',
-      url: '/login',
-      payload: {
-        username: 'sam',
-        password: 'pass'
-      }
-    };
-
-    server.inject(registerOptions, (registerRes) => {
-      t.equal(registerRes.statusCode, 200);
-      t.ok(registerRes.payload.includes('Hello Register'));
-      server.inject(loginOptions, (loginRes) => {
-        t.equal(loginRes.statusCode, 302);
-        t.equal(loginRes.headers.location, '/');
-        t.equal(loginRes.headers['set-cookie'][0].indexOf('cookie'), 0);
-        t.end();
-      });
-    });
-  });
+  flushDb()
+    .then(() => {
+      return server.inject(opts);
+    })
+    .then((res) => {
+      t.equal(res.statusCode, 400, 'opts with no payload');
+      const optsWoPass = Object.assign(opts, { payload: { username: 'sam' } });
+      return server.inject(optsWoPass);
+    })
+    .then((res) => {
+      t.equal(res.statusCode, 400, 'opts without a pass');
+      const optsWoUser = Object.assign(opts, { payload: { password: 'pass' } });
+      return server.inject(optsWoUser);
+    })
+    .then((res) => {
+      t.equal(res.statusCode, 400, 'opts without a user');
+      t.end();
+    })
+    .catch((err) => assert(!err, err));
 });
 
-tape('GET :: / without a cookie', (t) => {
-  const options = {
+tape('/register :: POST with an available username', (t) => {
+  const opts = {
+    method: 'post',
+    url: '/register',
+    payload: { username: 'sam', password: 'pass' }
+  };
+
+  flushDb()
+    .then(() => server.inject(opts))
+    .then((res) => {
+      t.equal(res.statusCode, 200);
+      t.ok(JSON.parse(res.payload).inserted);
+      return checkUserRegistered('sam')
+    })
+    .then((res) => {
+      t.ok(res);
+      t.end();
+    })
+    .catch((err) => assert(!err, err));
+});
+
+tape('/register :: POST with an unavaliable username', (t) => {
+  const opts = {
+    method: 'post',
+    url: '/register',
+    payload: { username: 'sam', password: 'pass' }
+  };
+
+  flushDb()
+    .then(() => registerUser({ username: 'sam', password: 'pass' }))
+    .then(() => checkUserRegistered('sam'))
+    .then((res) => {
+      t.ok(res);
+    })
+    .then(() => server.inject(opts))
+    .then((res) => {
+      t.equal(res.statusCode, 200);
+      t.notOk(JSON.parse(res.payload).inserted);
+      t.end();
+    })
+});
+
+tape('/login :: POST with no payload or payload without user or pass', (t) => {
+  const opts = { method: 'post', url: '/login' };
+
+  flushDb()
+    .then(() => {
+      return server.inject(opts);
+    })
+    .then((res) => {
+      t.equal(res.statusCode, 400, 'opts with no payload');
+      const optsWoPass = Object.assign(opts, { payload: { username: 'sam' } });
+      return server.inject(optsWoPass);
+    })
+    .then((res) => {
+      t.equal(res.statusCode, 400, 'opts without a pass');
+      const optsWoUser = Object.assign(opts, { payload: { password: 'pass' } });
+      return server.inject(optsWoUser);
+    })
+    .then((res) => {
+      t.equal(res.statusCode, 400, 'opts without a user');
+      t.end();
+    })
+    .catch((err) => assert(!err, err));
+});
+
+tape('/login :: POST with unregistered user', (t) => {
+  const opts = {
+    method: 'post',
+    url: '/login',
+    payload: { username: 'sam', password: 'pass' }
+  };
+
+  flushDb()
+    .then(() => {
+      return server.inject(opts);
+    })
+    .then((res) => {
+       t.equal(res.statusCode, 401);
+       t.notOk(JSON.parse(res.payload).login);
+       t.end();
+    })
+    .catch((err) => assert(!err, err));
+});
+
+tape('/login :: POST with wrong password', (t) => {
+  const opts = {
+    method: 'post',
+    url: '/login',
+    payload: { username: 'sam', password: 'notpass' }
+  };
+
+  flushDb()
+    .then(() => registerUser({ username: 'sam', password: 'pass' }))
+    .then(() => server.inject(opts))
+    .then((res) => {
+      t.equal(res.statusCode, 401);
+      t.notOk(JSON.parse(res.payload).login);
+      t.end();
+    })
+    .catch((err) => assert(!err, err));
+});
+
+// perhaps change this to re-log them back in
+tape('/login :: POST with already logged in user', (t) => {
+  const opts = {
+    method: 'post',
+    url: '/login',
+    payload: { username: 'sam', password: 'pass' }
+  };
+
+  flushDb()
+    .then(() => registerUser({ username: 'sam', password: 'pass' }))
+    .then(() => loginUser({ username: 'sam', password: 'pass' }))
+    .then(() => server.inject(opts))
+    .then((res) => {
+      t.equal(res.statusCode, 200);
+      t.ok(JSON.parse(res.payload).login);
+      t.equal(JSON.parse(res.payload).message, 'already logged in');
+      t.end();
+    })
+    .catch((err) => assert(!err, err));
+});
+
+tape('/login :: POST with correct creds not already logged in and a registered user', (t) => {
+  const opts = {
+    method: 'post',
+    url: '/login',
+    payload: { username: 'sam', password: 'pass' }
+  };
+
+  flushDb()
+    .then(() => registerUser({ username: 'sam', password: 'pass' }))
+    .then(() => server.inject(opts))
+    .then((res) => {
+      t.equal(res.statusCode, 200);
+      t.ok(JSON.parse(res.payload).login);
+      t.equal(JSON.parse(res.payload).message, 'logging in');
+      t.equal(res.headers['set-cookie'][0].indexOf('cookie='), 0);
+      redisCli.getAsync('sam')
+        .then((key) => {
+          const cookie = res.headers['set-cookie'][0].split('cookie=')[1];
+          t.equal(JSON.parse((new Buffer(cookie, 'base64')).toString()).key, key);
+          t.end();
+        })
+        .catch((error) => assert(!error, error));
+    })
+    .catch((err) => assert(!err, err));
+});
+
+tape('/logout :: POST with no cookies in the headers', (t) => {
+  const opts = {
+    method: 'post',
+    url: '/logout'
+  };
+
+  flushDb()
+    .then(() => server.inject(opts))
+    .then((res) => {
+      t.equal(res.statusCode, 400);
+      return server.inject(Object.assign(opts, { headers: {} }));
+    })
+    .then((res) => {
+      t.equal(res.statusCode, 400);
+      t.end();
+    })
+    .catch((err) => assert(!err, err));
+});
+
+tape('/logout :: POST should read the cookie, unstate it and logout with redis', (t) => {
+  const key = '267f666bf2496696fa299e20e6c3e6d84c4ac0fafdc1d389d585fc65c2606a7e';
+  const userObj = { username: 'sam', key: key };
+  const cookie = Buffer.from(JSON.stringify(userObj)).toString('base64')
+  const opts = {
+    method: 'post',
+    url: '/logout',
+    headers: {
+      'set-cookie': 'cookie=' + cookie
+    }
+  };
+
+  flushDb()
+    .then(() => redisCli.set('sam', key))
+    .then(() => checkUserLoggedIn('sam'))
+    .then((res) => {
+      t.ok(res);
+      return server.inject(opts);
+    })
+    .then((res) => {
+      t.equal(res.statusCode, 302);
+      return checkUserLoggedIn('sam');
+    })
+    .then((res) => {
+      t.notOk(res);
+      t.end();
+    })
+    .catch((err) => assert(!err, err));
+});
+
+tape('/ :: GET should redirect to /login if no cookie is present', (t) => {
+  const opts = {
     method: 'get',
     url: '/'
   };
 
-  server.inject(options, (res) => {
-    t.equal(res.statusCode, 302);
-    t.end();
-  });
+  flushDb()
+    .then(() => server.inject(opts))
+    .then((res) => {
+      t.equal(res.statusCode, 302);
+      t.equal(res.headers.location, '/login/logged_out=true');
+      t.end();
+    })
+    .catch((err) => assert(!err, err));
 });
 
-tape('GET :: / with incorrect cookie', (t) => {
-  flushDb(() => {
-    const registerOptions = {
-      method: 'post',
-      url: '/register',
-      payload: {
-        username: 'sam',
-        password: 'pass'
-      }
-    };
+tape('/ :: GET should redirect to /login with invalid cookie', (t) => {
+  const opts = {
+    method: 'get',
+    url: '/',
+    headers: {
+      'set-cookie': 'notacookie'
+    }
+  };
 
-    const loginOptions = {
-      method: 'post',
-      url: '/login',
-      payload: {
-        username: 'sam',
-        password: 'pass'
-      }
-    };
-
-    const homeOptions = {
-      method: 'get',
-      url: '/',
-      headers: {}
-    };
-
-    server.inject(registerOptions, (registerRes) => {
-      t.equal(registerRes.statusCode, 200);
-      t.ok(registerRes.payload.includes('Hello Register'));
-
-      server.inject(loginOptions, (loginRes) => {
-        t.equal(loginRes.statusCode, 302);
-        t.equal(loginRes.headers.location, '/');
-
-        const creds = { user: 'sam', pass: 'notpass' };
-        const wrongCookie = new Buffer(JSON.stringify(creds)).toString('base64');
-
-        homeOptions.headers.cookie = 'cookie=' + wrongCookie;
-
-        server.inject(homeOptions, (homeRes) => {
-          t.equal(homeRes.statusCode, 401);
-          t.equal(homeRes.payload, 'Incorrect password');
-          t.end();
-        });
-      });
-    });
-  });
+  flushDb()
+    .then(() => server.inject(opts))
+    .then((res) => {
+      t.equal(res.statusCode, 302);
+      t.equal(res.headers.location, '/login/logged_out=true');
+      t.end();
+    })
+    .catch((err) => assert(!err, err));
 });
 
-tape('GET :: / after setting cookie', (t) => {
-  flushDb(() => {
-    const registerOptions = {
-      method: 'post',
-      url: '/register',
-      payload: {
-        username: 'sam',
-        password: 'pass'
-      }
-    };
+tape('/ :: GET should redirect to /login with wrong cookie', (t) => {
+  const opts = {
+    method: 'get',
+    url: '/',
+    headers: {
+      'set-cookie': 'cookie=wrongcookie'
+    }
+  };
 
-    const loginOptions = {
-      method: 'post',
-      url: '/login',
-      payload: {
-        username: 'sam',
-        password: 'pass'
-      }
-    };
+  flushDb()
+    .then(() => server.inject(opts))
+    .then((res) => {
+      t.equal(res.statusCode, 302);
+      t.end();
+    })
+    .catch((err) => assert(!err, err));
+});
 
-    const homeOptions = {
-      method: 'get',
-      url: '/',
-      headers: {}
-    };
+tape('/ :: GET should redirect to /login for being stored in redis, but incorrect cookie key', (t) => {
+  const opts = {
+    method: 'get',
+    url: '/',
+    headers: {
+      'set-cookie': 'cookie=eyJ1c2VybmFtZSI6InNhbSIsImtleSI6IkhNZUs5WE1sZW9DbTlhWmJrTWZSVEZsNThRVTNWbFhlYURlaktRVnN6UzJvWG45YVRVMlJ5VGVySHVqdVhRcHNqRzA5NjZXelRQT0hwVm95bTMyL05nY0toN085elhhV1VmZFlNV1NEUUNra1BMWHU0eUJGNWFLdFE3Umd4eTFNN0pXakE4a3d5a1F rNTFPdE8yRGVWMklFYnBrbDZPV00rSGRKUmFBNk1IYW1IU0srYVdUOHQ3SGM3QkJudXBIWFhqWkNFa3pDY290Tk5COUwzMjNsR3VMSktoL2VvTU1zL1IyZmtzaTJONm5sUk95SFNYWjVpNzhyN2ZvY2l1OHZKajQ4cnhBOG9UNFZ3OERnTVJoZERXczc0M1dhUHJJUWh3MU95WjBQQVBDTzNxZUpicW1XZzFZaFl KZmlQTzd1VW1mN2hlMmdlbWFIU09CVlkxWloxUT09In0=; HttpOnly; SameSite=Strict'
+    }
+  };
 
-    server.inject(registerOptions, (registerRes) => {
-      t.equal(registerRes.statusCode, 200);
-      t.ok(registerRes.payload.includes('Hello Register'));
-      server.inject(loginOptions, (loginRes) => {
-        t.equal(loginRes.statusCode, 302);
-        t.equal(loginRes.headers.location, '/');
-        homeOptions.headers.cookie = loginRes.headers['set-cookie'][0].split(';')[0]
-        server.inject(homeOptions, (homeRes) => {
-          t.equal(homeRes.statusCode, 200);
-          t.ok(homeRes.payload.includes('Hello Home'));
-          t.end();
-        });
-      });
-    });
-  });
+  const loginOpts = {
+    method: 'post',
+    url: '/login',
+    payload: {
+      username: 'sam',
+      password: 'pass'
+    }
+  };
+
+  flushDb()
+    .then(() => registerUser({ username: 'sam', password: 'pass' }))
+    .then(() => server.inject(loginOpts))
+    .then((res) => {
+      t.equal(res.statusCode, 200);
+      return server.inject(opts);
+    })
+    .then((res) => {
+      t.equal(res.statusCode, 302);
+      t.equal(res.headers.location, '/login/logged_out=true');
+      t.end();
+    })
+    .catch((err) => assert(!err, err));
+});
+
+tape('/ :: GET should reach / when logged in properly', (t) => {
+  const loginOpts = {
+    method: 'post',
+    url: '/login',
+    payload: {
+      username: 'sam',
+      password: 'pass'
+    }
+  };
+
+  const opts = {
+    method: 'get',
+    url: '/'
+  };
+
+  let cookie;
+
+  flushDb()
+    .then(() => registerUser({ username: 'sam', password: 'pass' }))
+    .then(() => server.inject(loginOpts))
+    .then((res) => {
+      t.equal(res.statusCode, 200);
+      t.ok(res.headers['set-cookie'][0]);
+      cookie = res.headers['set-cookie'][0];
+      const optsWithCookie = Object.assign(opts, { headers: { 'set-cookie': cookie } });
+      return server.inject(optsWithCookie);
+    })
+    .then((res) => {
+      t.equal(res.statusCode, 200);
+      t.ok(res.payload.includes('Hello Home'));
+      t.end();
+    })
+    .catch((err) => assert(!err, err));
 });
 
 tape.onFinish(() => {
-  server.app.redisCli.flushall(() => {
-    flushDb(() => {
+  flushDb()
+    .then(() => {
       server.app.redisCli.quit();
       server.app.pool.end();
     });
-  });
 });
