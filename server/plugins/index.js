e72775280ef0993302964743efe2903c7654b666
Sam Houston <shouston3@me.com>
2016-10-13 21:40:27 +0100

    Renaming plugins

--- server/plugins/index.js
+++ server/plugins/index.js
@@ -1,17 +1,17 @@
 const redis = require('./redis.js');
 const postgres = require('./postgres.js');
-const scheme = require('./scheme.js');
-const strategy = require('./strategy.js');
 const register = require('./register.js');
 const login = require('./login.js');
-const clearCookies = require('./clearCookies.js');
+const logout = require('./logout.js');
+const scheme = require('./scheme.js');
+const strategy = require('./strategy.js');
 
 module.exports = [
   redis,
   postgres,
-  scheme,
-  strategy,
   register,
   login,
-  clearCookies
+  logout,
+  scheme,
+  strategy
 ];
