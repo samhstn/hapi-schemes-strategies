3ea9b33ecd5b740654ae61f2cf8ea217e1161b6e
Sam Houston <shouston3@me.com>
2016-10-14 18:22:26 +0100

    Requiring in my db config files instead of just repeating my configuration for them while improving coverage

--- server/plugins/redis.js
+++ server/plugins/redis.js
@@ -1,16 +1,11 @@
 const redis = require('redis');
 const bluebird = require('bluebird');
+const opts = require('../../redisConfig.js');
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
 exports.register = function (server, options, next) {
-  const opts = {
-    host: '127.0.0.1',
-    port: 6379,
-    db: process.env.NODE_ENV === 'test' ? 1 : 0
-  };
-
   server.app.redisCli = redis.createClient(opts);
   
   next();
